e043852694f95423d0e1916695a3e15570779b91
曹合飞 <caohefei@gokuaidian.com>
2020-04-18 16:15:01 +0800

    add copyright

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,7 +8,7 @@
 
 import React, {useState}from 'react';
 import Input from '../../components/input'
-import './index.css'
+import './index.less'
 function Index ({routes, history}){
   const [email, setEmail]  =  useState('')
   const [password, setPassword]  =  useState('')
@@ -23,8 +23,8 @@ function Index ({routes, history}){
   <div className='login'>
       <div  className='mid_module'>
           <div className='logo'></div>
-          <div  className='creatUser'>New to Haibilin? <span className='creatbtn' onClick={goResgister}  >  Create an account. </span></div>
-          <div className='des_welcome'>Welcome to Haibilin</div>
+          <div  className='creatUser'>New to Hiblim? <span className='creatbtn' onClick={goResgister}  >  Create an account. </span></div>
+          <div className='des_welcome'>Welcome to Hiblim</div>
           <div className='content'>
              <Input  type='email' value={email} onChange={val=>setEmail(val)} { ... {title:'请输入邮箱'}}  />
              <Input  type='password' value={password} onChange={val=>setPassword(val)}   { ... {title:'请输入密码'}} />
@@ -35,6 +35,7 @@ function Index ({routes, history}){
           </div>
 
       </div>
+   <div class="copyright"><a href="http://www.beian.miit.gov.cn/">苏ICP备20013765号-1 </a> | 版权 © 2020 Nanjing Yunfengsu Technology Co., Ltd. | 电话: 4006-922-006 </div>
   </div>
   )
   }
