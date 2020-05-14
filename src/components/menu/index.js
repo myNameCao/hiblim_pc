f04419bfff5c0862e8225fc286a4cec998ef83cd
曹合飞 <caohefei@gokuaidian.com>
2020-05-14 23:16:03 +0800

    revise host

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -3,7 +3,7 @@
  * @Author: @[caohefei]
  * @Date: 2020-03-31 16:10:07
  * @LastEditors: @[caohefei]
- * @LastEditTime: 2020-05-14 14:03:57
+ * @LastEditTime: 2020-05-14 22:14:49
  */
 
 import React from 'react';
@@ -18,12 +18,15 @@ function Head () {
             <a href="index.html"><img src={logo} /></a>
             </h1>
          	 <ul>
-   	 	<li><a href="http://hk.hiblim.com/incorporation.html">COMPANY REGISTRATION</a></li>
+   	 	<li><a href="http://www.hiblim.com/incorporation.html">COMPANY REGISTRATION</a></li>
 		<li>
-			<a href="http://hk.hiblim.com/secretary.html">COMPANY SECRETARY</a>
+			<a href="http://www.hiblim.com/secretary.html">COMPANY SECRETARY</a>
 		</li>
    	<li>
-			<a href="http://hk.hiblim.com/accounting.html">ACCOUNTING & AUDITING</a>
+			<a href="http://www.hiblim.com/accounting.html">ACCOUNTING & AUDITING</a>
+		</li>
+   	<li>
+			<a >CONTACT US</a>
 		</li>
    	 </ul>
        </div>	
