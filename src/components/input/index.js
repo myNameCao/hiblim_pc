aa8f70410a516dfe446443091672b754976a56b6
曹合飞 <caohefei@gokuaidian.com>
2020-05-26 17:16:59 +0800

    add file page style

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -3,7 +3,7 @@
  * @Author: @[caohefei]
  * @Date: 2020-03-31 16:10:07
  * @LastEditors: @[caohefei]
- * @LastEditTime: 2020-05-11 11:18:20
+ * @LastEditTime: 2020-05-26 17:16:09
  */
 
  
@@ -13,10 +13,11 @@ import React, {useState} from 'react'
 
 import './index.css'
 
-function Input ({title, type, value, onChange, check}) {
+function Input ({title, type, value, onChange, check, allowEmpty}) {
   const [currentState, setCurrentState]  = useState('') // error focus
   function blur (e){
-    if(!e.target.value){
+    console.log(allowEmpty)
+    if(!e.target.value && !allowEmpty){
       setCurrentState('error')
       check && check(false)
       return 
@@ -30,7 +31,7 @@ function Input ({title, type, value, onChange, check}) {
     setCurrentState('')
   }
   return (
-    <div className={`inputItem ${currentState}`}>
+    <div className={`inputItem_component ${currentState}`}>
             <p>{title}</p>
             <input  autoComplete='new-password' onFocus={e=>setCurrentState('focus')}  onBlur={e=>blur(e)} type={type || 'text' } value={value}  onChange={e=>onChange && onChange(e.target.value)} />
     </div>
--- src/pages/files/index.js
+++ src/pages/files/index.js
@@ -2,12 +2,19 @@ import React, {useState} from 'react'
 
 import fileImg from './files.png'
 
+import Input from '../../components/input'
+
+
 import './index.less'
 
 
  function Files (){
-    const [paths, setPaths] = useState('cao')
-    const [filesList, setFilesList] = useState([{name:'capo'}])
+    const [paths, setPaths] = useState()
+    const [filesList, setFilesList] = useState([
+      {name:'Accounting', type:'file', time:'Apr 12, 2020 14:48:34', id:1},
+      {name:'Corporate ', type:'dir', time:'Apr 12, 2020 14:48:34', id:2}
+    ])
+    const [allsel, setAllsel] = useState(false)
 
    return (
      <div className='files' >
@@ -17,19 +24,22 @@ import './index.less'
          </div>
        <div className='searchFiles'>
          <div className='icon-search'></div>
+         <div className='searchInput'>
+            <Input  title='search' allowEmpty />
+         </div>
        </div>
       <div className='path'>Secretary  {paths ? `> ${paths}` : ''}</div>
        <ul className='filesView'>
           <li className='fileslistTitle fileLine'>
-           <span  className='select_icon' ></span>
-           <span  className='fileType_icon icon-alldir' ></span>
+          <span  className={allsel ? 'fileSel icon-select' : 'fileSel icon-notsel'} ></span>
+           <span  className='fileType icon-alldir' ></span>
            <span  className='FilesName' >Name</span>
           </li>
           {filesList.map(file=>{
-            return <li className='filesItem  fileLine' key={file}>
+            return <li className='filesItem  fileLine' key={file.id}>
                       <span  className={file.type ? 'fileSel icon-select' : 'fileSel icon-notsel'} ></span>
-                      <span  className={file.type ? 'fileType icon-file' : 'fileType icon-alldir'} ></span>
-                      <span  className='FilesName' >{file.name}</span>
+                      <span  className={file.type === 'file' ? 'fileType icon-file' : 'fileType icon-folder'} ></span>
+                      <span  className='filesName' >{file.name}</span>
                       <span  className='filesTime' >{file.time}</span>
            </li>
           })}
